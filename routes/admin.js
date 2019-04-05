47b4335e3f3749204c33b0aacbb5b92d3b53fba5
psy901 <psy901@gmail.com>
2019-04-05 14:21:02 -0400

    add admin template views. add admin route configuration

--- routes/admin.js
+++ routes/admin.js
@@ -6,37 +6,44 @@ router.get('/', (req, res) => {});
 
 // Screen 18
 router.get('/users', (req, res) => {
-  res.send('Screen 18');
+  // res.send('Screen 18');
+  res.render('admin/users', { title: 'Users'});
 });
 
 // Screen 19
 router.get('/sites', (req, res) => {
-  res.send('Screen 19');
+  // res.send('Screen 19');
+  res.render('admin/sites', { title: 'Sites' });
 });
 
 // Screen 21
 router.get('/sites/create', (req, res) => {
-  res.send('Screen 21');
+  // res.send('Screen 21');
+  res.render('admin/create-sites', { title: 'Create Sites' });
 });
 
 // Screen 20
 router.get('/sites/:id/edit', (req, res) => {
-  res.send('Screen 20');
+  // res.send('Screen 20', {title: 'Edit Site'});
+  res.render('admin/edit-site', { title: 'Edit Site' });
 });
 
 // Screen 22
 router.get('/transits', (req, res) => {
-  res.send('Screen 22');
+  // res.send('Screen 22', { title: 'Transits' });
+  res.render('admin/transits', { title: 'Transits' });
 });
 
 // Screen 24
 router.get('/transits/create', (req, res) => {
-  res.send('Screen 24');
+  // res.send('Screen 24', {title: 'Create Transit'});
+  res.render('admin/create-transit', { title: 'Create Transit' });
 });
 
 // Screen 23
 router.get('/transits/:id/edit', (req, res) => {
-  res.send('Screen 23');
+  // res.send('Screen 23', { title: 'Edit Transit' });
+  res.render('admin/edit-transit', { title: 'Edit Transit'});
 });
 
 export default router;
