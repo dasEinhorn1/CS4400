76dbb82edc2c41d5a7d3ec888318b4d84b7a2733
Adam Hayward <achayward1@gmail.com>
2019-04-08 15:15:22 -0400

    set up manager views

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import AdminRoutes from './routes/admin';
 const app = express();
 const port = 3000;
 
-
-
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index', {title:"heyo"}));
 app.use('/manager', ManagerRoutes);
--- routes/manager.js
+++ routes/manager.js
@@ -4,32 +4,37 @@ const router = express.Router();
 
 // Screen 25
 router.get('/events', (req, res, next) => {
-  res.send('Screen 25')
+  res.render('manager/events', {title:"Piedmont Park Events"})
 })
 
 // Screen 27
 router.get('/events/create', (req, res, next) => {
-  res.send('Screen 27')
+  res.render('manager/create-event', {title:"Create Event"})
+})
+
+router.post('/events/create', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/manager/events')
 })
 
 // Screen 26
 router.get('/events/:id/edit', (req, res, next) => {
-  res.send('Screen 26')
+  res.render('manager/edit-event', {title:"Edit Event"})
 })
 
 // Screen 28
 router.get('/events/staff', (req, res, next) => {
-  res.send('Screen 28')
+  res.render('manager/event-staff', {title:"Event Staff"})
 })
 
 // Screen 29
 router.get('/site', (req, res, next) => {
-  res.send('Screen 29')
+  res.render('manager/site', {title:"Sites"})
 })
 
 // Screen 30
 router.get('/site/detail', (req, res, next) => {
-  res.send('Screen 30')
+  res.render('manager/site-detail', {title:"Site Detail"})
 })
 
 export default router;
