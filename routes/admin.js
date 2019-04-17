edf4dc6bcd76d7c9171d80afe62001058c4f4666
Adam Hayward <achayward1@gmail.com>
2019-04-16 20:53:46 -0400

    removes broken admin default route

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+// router.get('/', (req, res) => {});
 
 // Screen 18
 router.get('/users', (req, res) => {
