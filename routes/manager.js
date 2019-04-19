4ae0f3e124b52cca1bcd175a98353067832535ad
Adam Hayward <achayward1@gmail.com>
2019-04-18 20:48:16 -0400

    prepares remaining manager routes for database integration

--- routes/manager.js
+++ routes/manager.js
@@ -200,17 +200,109 @@ router.post('/events/create', (req, res, next) => {
 
 // Screen 28
 router.get('/events/staff', (req, res, next) => {
-  res.render('manager/event-staff', {title:"Event Staff"})
+  // get the name of the manager's site as siteName
+  const siteName = "Piedmont Park";
+
+  const firstName = req.query.firstName;
+  const lastName = req.query.lastName;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
+  const staff = [
+    {
+      firstName: "Adam",
+      lastName: "Hayward",
+      username: "ahayward3",
+      eventShifts: 4,
+    },
+    {
+      firstName: "MJ",
+      lastName: "Park",
+      username: "mjpark",
+      eventShifts: 5,
+    }
+  ];
+
+  res.render('manager/event-staff', {
+    site: { name: siteName },
+    staff,
+    formValues: {
+      firstName,
+      lastName,
+      startDate,
+      endDate
+    }
+  })
 })
 
 // Screen 29
 router.get('/site', (req, res, next) => {
-  res.render('manager/site', {title:"Sites"})
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+  const lowerEventCount = req.query.lowerEventCount;
+  const upperEventCount = req.query.upperEventCount;
+  const lowerStaffCount = req.query.lowerStaffCount;
+  const upperStaffCount = req.query.upperStaffCount;
+  const lowerVisits = req.query.lowerVisits;
+  const upperVisits = req.query.upperVisits;
+  const lowerRevenue = req.query.lowerRevenue;
+  const upperRevenue = req.query.upperRevenue;
+
+  const dailies = [
+    {
+      date: '2019-01-23',
+      eventCount: 2,
+      staffCount: 1,
+      visits: 4,
+      revenue: 3
+    }
+  ]
+
+  res.render('manager/site', {
+    dailies,
+    formValues: {
+      startDate,
+      endDate,
+      lowerEventCount,
+      upperEventCount,
+      lowerStaffCount,
+      upperStaffCount,
+      lowerVisits,
+      upperVisits,
+      lowerRevenue,
+      upperRevenue
+    }
+  })
 })
 
 // Screen 30
 router.get('/site/detail', (req, res, next) => {
-  res.render('manager/site-detail', {title:"Site Detail"})
+  // get the site from the current manager
+  const siteName = "Piedmont Park";
+
+  const date = req.query.date;
+
+  // get each event name, the staff for that day,
+  // the daily revenue and visit total
+
+  const eventDailies = [
+    {
+      name: "Arboretum Walking Tour",
+      staff: ["ahayward1"],
+      visits: 34,
+      revenue: 0
+    },
+    {
+      name: "Bus Tour",
+      staff: ["mjpark"],
+      visits: 13,
+      revenue: 13 * 5
+    }
+  ]
+
+  res.render('manager/site-detail', {
+    eventDailies
+  })
 })
 
 export default router;
